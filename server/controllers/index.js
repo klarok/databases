d631f1d6ffdd29a4c76f08118983096e6cb052d9
klarok <karaylu@gmail.com>
2018-12-06 15:12:21 -0800

    Have client connected, hardcoded queries work

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,14 +1,21 @@
+var Promise = require('bluebird');
 var models = require('../models');
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      let results = models.messages.get();
-      res.send('got results');
+      models.messages.get((err, messagesData) => {
+        if (err) throw err;
+        //console.log(messagesData);
+        res.send(JSON.stringify(messagesData));
+      });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      console.log(req.body, 'messages');
-      res.send('messages post');
+      console.log(req.body, 'post messages');
+      models.messages.post((err) => {
+        if (err) throw err;
+        res.send('message posted');
+      });
     } // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,18 +2,23 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {
+    get: function (callback) {
       let q = `SELECT users.username, rooms.roomname, messages.body FROM messages
                 INNER JOIN users ON users.id = messages.user_id
                 INNER JOIN rooms ON rooms.id = messages.room_id`;
-      let results = db.query(q, (err, results, fields) => {
+      db.query(q, (err, results, fields) => {
         if (err) throw err;
-        console.log(results, 'models');
-        return results;
+        callback(null, results);
       });
-      return results;
     }, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function (callback) {
+      let q = `INSERT INTO messages VALUES (default, 1, 1, 'with the Fish', default);`;
+      db.query(q, (err, data) => {
+        if (err) throw err;
+        console.log(data, 'post query to database');
+        callback(null);
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
@@ -22,4 +27,5 @@ module.exports = {
     post: function () {}
   }
 };
+
 
