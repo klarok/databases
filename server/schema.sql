dc1e103d17b93fb8e2b5a7743dc5f2975eddb07b
klarok <karaylu@gmail.com>
2018-12-05 21:01:41 -0800

    Created database chat

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(20)
+);
+
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  roomName VARCHAR(20)
+);
+
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  user_id INTEGER,
+  room_id INTEGER,
+  body TEXT,
+  created TIMESTAMP,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
